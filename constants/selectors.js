e911e68d6d845458618f46cd1e776e05c438a1f0
Isabelle Anderson-Gregoire <ianderson.gregoire@gmail.com>
2025-08-17 23:40:05 -0500

    removed inactive files

--- constants/selectors.js
+++
@@ -1,8 +0,0 @@
-export const SELECTORS = {
-	VIDEO: "video",
-	FULLSCREEN_ELEMENT: () => document.fullscreenElement,
-	BLOCKING_ELEMENTS: "body *:not(:fullscreen)",
-	PROFILE_CHOICE: "profile-gate-label",
-	//PROFILE_CHOICE: () => document.getElementsByClassName("profile-gate-label"),
-  };
-  
